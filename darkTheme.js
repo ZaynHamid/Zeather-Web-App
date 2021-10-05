f9d84b356eddafa991cd33faedbd14c98acc4970
Zuzu <zainhamid982@gmail.com>
2021-10-05 16:58:08 +0500

    Commited all files

---
+++ darkTheme.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  function hi() {
+    //  document.getElementById("x").classList.toggle("dark"); 
+    document.getElementById("c").classList.toggle("app");
+    document.getElementById("c").classList.toggle("lun");
+    document.getElementById("main").classList.toggle("dark");
+    document.getElementById("nav").classList.toggle("dark");
+    document.getElementById("nav").classList.toggle("default");
+  }
